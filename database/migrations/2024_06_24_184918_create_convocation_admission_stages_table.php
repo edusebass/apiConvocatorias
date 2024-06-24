91c597c7c610fd30fe9c4991e40f1fb81a83e106
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-06-24 14:10:47 -0500

    migraciones admision stages | convocation stages

---
+++ database/migrations/2024_06_24_184918_create_convocation_admission_stages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('convocation_admission_stages', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('admission_stage_id');
+            $table->uuid('convocation_id');
+            $table->tinyInteger('step');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->uuid('updated_by')->nullable();
+            $table->uuid('created_by')->nullable();
+            $table->uuid('deleted_by')->nullable();
+
+            $table->foreign('admission_stage_id')->references('id')->on('admission_stages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('convocation_admission_stages');
+    }
+};
